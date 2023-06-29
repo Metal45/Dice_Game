5c5f43bd41bb28f9a88d741c20feedf7691775e8
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-06-30 00:17:09 +0530

    Dice_Game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var dices = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var dices = ["image/dice1.png", "image/dice2.png", "image/dice3.png", "image/dice4.png", "image/dice5.png", "image/dice6.png"];
 
 document.querySelector("img.img1").src = dices[randomNumber1];
 document.querySelector("img.img2").src = dices[randomNumber2];
